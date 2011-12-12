2e9a663962862f245885ebc7f150ce969224219a
Eric Lee <tsheep.lee@gmail.com>
2011-12-12 16:11:02 +0800

    add field

--- src/main/java/com/github/airqs/entity/Station.java
+++ src/main/java/com/github/airqs/entity/Station.java
@@ -9,8 +9,9 @@ package com.github.airqs.entity;
  */
 public class Station extends BaseEntity {
 	private Integer id;
-	private String name;
 	private String areaName;
+	private String name;
+	private String pointName;
 	private Integer cityId;
 	public Integer getId() {
 		return id;
@@ -30,25 +31,19 @@ public class Station extends BaseEntity {
 	public void setAreaName(String areaName) {
 		this.areaName = areaName;
 	}
+	
+	public String getPointName() {
+		return pointName;
+	}
+	public void setPointName(String pointName) {
+		this.pointName = pointName;
+	}
 	public Integer getCityId() {
 		return cityId;
 	}
 	public void setCityId(Integer cityId) {
 		this.cityId = cityId;
 	}
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Station [id=");
-		builder.append(id);
-		builder.append(", name=");
-		builder.append(name);
-		builder.append(", areaName=");
-		builder.append(areaName);
-		builder.append(", cityId=");
-		builder.append(cityId);
-		builder.append("]");
-		return builder.toString();
-	}
+
 	
 }
